bb84b1d2ec90562a26f3e80cffaf8fe6dbdf2cb4
Alex <daos147@gmail.com>
2020-07-02 16:07:04 +0300

    add form validate, mask number, send on email data and add mobile adaptive head text

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-//hamburger
+//hamburger menu
 const menu = document.querySelector('.menu'),
 menuItem = document.querySelectorAll('.menu_item'),
 hamburger = document.querySelector('.hamburger');
@@ -16,7 +16,7 @@ menuItem.forEach(item => {
     })
 });
 
-//Modal 
+//Modal windows
 $('[data-modal=consultation]').on('click', function() {
     $('.overlay, #consultation').fadeIn('slow');
   });
@@ -28,6 +28,59 @@ $('[data-modal=consultation]').on('click', function() {
       $('#order .modal__descr').text($('.services-item__descr').eq(i).text());
       $('.overlay, #order').fadeIn('slow');
     });
-  });      
+  }); 
+  
+  //Validate form
+  
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+          maxlength: 50
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа")
+        },
+        phone: "Пожалуйста введите свой номер телефона",
+        email: {
+          required: "Пожалуйста введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  };
+  validateForms('#signup-form_id');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+  //phone number entry Mask
+  $('input[name=phone]').mask("+38 (099) 999-99-99");
+
+//send email whith site
+$('form').submit(function(e) {
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "mailer/smart.php",
+    data: $(this).serialize()
+  }).done(function() {
+    $(this).find("input", "textarea").val("");
+    $('#consultation, #order').fadeOut();
+    $('.overlay, #thanks').fadeIn('slow');
+
+    $('form').trigger('reset');
+  });
+  return false;
+});
 
 });
