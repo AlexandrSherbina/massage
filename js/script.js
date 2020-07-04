465450bed84abfcb1c1834b021edbe698696d77f
Alex <daos147@gmail.com>
2020-07-04 13:04:57 +0300

    add scrll page up

--- js/script.js
+++ js/script.js
@@ -82,5 +82,19 @@ $('form').submit(function(e) {
   });
   return false;
 });
+// scroll page up
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1200 ) {
+    $('.pageup').fadeIn();
+  } else {
+    $('.pageup').fadeOut();
+  }
+});
+
+$("a[href^='#']").click(function(){
+  const _href = $(this).attr("href");
+  $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+  return false;
+});
 
 });
