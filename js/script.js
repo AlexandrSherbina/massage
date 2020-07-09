de85ba3caffcfcbbc427f04409f37ed5531f5963
Alex <daos147@gmail.com>
2020-07-09 15:18:28 +0300

    Комментарий к ветке branch_name

--- js/script.js
+++ js/script.js
@@ -84,7 +84,7 @@ $('form').submit(function(e) {
 });
 // scroll page up
 $(window).scroll(function() {
-  if ($(this).scrollTop() > 1200 ) {
+  if ($(this).scrollTop() > 1000 ) {
     $('.pageup').fadeIn();
   } else {
     $('.pageup').fadeOut();
@@ -97,25 +97,63 @@ $("a[href^='#']").click(function(){
   return false;
 });
 // slick slider
-$(document).ready(function () {
-  $('.carousel__inner').slick({
-    infinite: true,
-    speed: 900, 
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.svg"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {          
-        arrows: false
-        }
-      }
-    ]     
-  });
+// $(document).ready(function () {
+//   $('.carousel__inner').slick({
+//     infinite: true,
+//     speed: 900, 
+//     slidesToShow: 1,
+//     adaptiveHeight: true,
+//     // autoplay: true,
+//     // autoplaySpeed: 2000,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/left.svg"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.svg"></button>',
+//     responsive: [
+//       {
+//         breakpoint: 992,
+//         settings: {          
+//         arrows: false
+//         }
+//       }
+//     ]     
+//   });
+// });
+
+// tiny slider 
+const slider = tns({
+  container: '.carousel__inner',
+  infinite: true,
+  center: true,
+  loop: true,
+  items: 1,
+  touch: true,
+  slideBy: "page",
+  speed: 1600,
+  mouseDrag: true,
+  swipeAngle: false,
+  autoWidth: true,
+  autoplay: false,
+  controls: false,
+  nav: false,
+  responsive: {
+    640: {    
+      edgePadding: 20,  
+      gutter: 20,
+      items: 1
+    },
+    767: {
+      gutter: 30
+    },
+    991: {
+      items: 1
+    }
+  }
+
+});
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
 
 });
